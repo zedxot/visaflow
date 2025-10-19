2f66d8ec630f7a332782d1c78cd1adfcf6667c1e
EnigmaticJihad <enigmatic.jihad@gmail.com>
2025-10-19 15:29:22 +0600

    after build

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './visaflow.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
